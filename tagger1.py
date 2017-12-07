8dea3b1eb2ad9bfeaa4870ff02c438c39fef8345
Tomer Gill <coolran103@gmail.com>
2017-12-07 13:26:32 +0200

    added prediction

--- tagger1.py
+++ tagger1.py
@@ -6,6 +6,7 @@ import torch.optim as opt
 import torch.utils.data as utdata
 import utils1 as ut
 import numpy as np
+import time
 
 
 # The Nueral Net
@@ -35,10 +36,13 @@ def make_data_loader(examples, batch_size=100, shuffle=True):
     return utdata.DataLoader(train, batch_size, shuffle)
 
 
-def train_net(net, data_loader, iter_num, optimizer, criterion):
-    print "it loss"
+def train_net(net, data_loader, iter_num, optimizer, criterion, acc_loader):
+    print "+----+--------+----------+----------+---------+"
+    print "| it |  loss  | time (s) | dev_loss | dev_acc |"
+    print "+----+--------+----------+----------+---------+"
     for i in range(iter_num):
         cum_loss = 0.0
+        start_time = time.time()
         for _, (inputs, labels) in enumerate(data_loader, 0):
             inputs, labels = Variable(inputs), Variable(labels)
             optimizer.zero_grad()
@@ -47,32 +51,89 @@ def train_net(net, data_loader, iter_num, optimizer, criterion):
             loss.backward()
             optimizer.step()
             cum_loss += loss.data[0]
-        print i, cum_loss / len(data_loader)
+        acc, loss = accuracy_and_loss_on(net, acc_loader, criterion)  # compute accuracy in each iteration
+        print "| %-2d | %1.4f | %8.5f | %f | %6.3f%% |" % (
+            i, cum_loss / len(data_loader), time.time() - start_time, loss, acc * 100)
+    print "+----+--------+----------+----------+---------+"
 
 
-def accuracy_on(net, data_loader):
-    good = total = 0.0
+def accuracy_and_loss_on(net, data_loader, criterion):
+    good = bad = cum_loss = 0.0
     for data in data_loader:
         feats, labels = data
+        ################################
+        # print feats
+        # print labels
+        # for j in range(0, 1000, 100):
+        #     temp = []
+        #     for i in range(5):
+        #         temp.append(ut.I2W[feats[j, i]])
+        #     print temp
+        #     print ut.I2T[labels[j]]
+        # input()
+        ################################
         outputs = net(Variable(feats))
         _, predicted = tr.max(outputs.data, 1)
-        total += labels.size(0)
-        good += (predicted == labels).sum()
-    return good / total
+        comp = (predicted == labels)
+        bad += (predicted != labels).sum()
+        if ut.dir_name == "ner":
+            comp = [0 if labels[i] == ut.T2I["O"] else score for i, score in enumerate(comp)]
+        good += sum(comp)
+        loss = criterion(outputs, Variable(labels))
+        cum_loss += loss.data[0]
+    return good / (good + bad), (cum_loss / len(data_loader))
+
+
+def predict_by_windows(net, windows):
+    prediction, inputs = []
+    for input in windows:
+        prediction += list(tr.max(net(Variable(input)).data, 1))
+        inputs += list(input[:, 2])
+    return prediction, inputs
 
 
 if __name__ == "__main__":
+    train = True
+    save_model = True
+    test = True
+    model_args_path = "~/Desktop/Deep Learning/ass2"
+    load_model = True
+
     EMBED_SIZE = 50
     WIN_SIZE = 5
     epcohes = 15
-    learning_rate = 0.01
+    learning_rate = 0.001
     batch_size = 1000
+    hidden_dim = 100
 
-    net = Net(len(ut.W2I), EMBED_SIZE, WIN_SIZE, 100, len(ut.T2I), batch_size)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = opt.SGD(net.parameters(), learning_rate)
+    net = Net(len(ut.W2I), EMBED_SIZE, WIN_SIZE, hidden_dim, len(ut.T2I), batch_size)
 
-    print "Learning on test:"
-    train_net(net, make_data_loader(ut.TRAIN, batch_size), epcohes, optimizer, criterion)
-    print "Accuracy on dev: %d %%" % (accuracy_on(net, make_data_loader(ut.DEV, batch_size)))
+    if load_model:
+        net.load_state_dict(tr.load(model_args_path))
+
+    if train:
+        criterion = nn.CrossEntropyLoss()
+        optimizer = opt.Adam(net.parameters(), learning_rate)
+
+        print "######################################################"
+        print "Run parameters:"
+        print "*\tEmbedding layer size: %d" % EMBED_SIZE
+        print "*\tWindow size: %d" % WIN_SIZE
+        print "*\tNumber of iterations: %d" % epcohes
+        print "*\tLearning rate: %f" % learning_rate
+        print "*\tBatch size: %d" % batch_size
+        print "*\tHidden dimension size: %d" % hidden_dim
+        print "######################################################"
+        print "\nLearning on test, with accuracy on dev each iteration:"
+        train_net(net, make_data_loader(ut.TRAIN, batch_size), epcohes, optimizer, criterion,
+                  make_data_loader(ut.DEV, batch_size))
+        if save_model:  # should save the net
+            tr.save(net.state_dict(), model_args_path)
+
+    if test:
+        predictions, inputs = predict_by_windows(net, ut.TEST)
+        pred_file = open("~/Desktop/Deep Learning/ass2/test1." + ut.dir_name, "w")
+        for i, pred in enumerate(predictions):
+            pred_file.write(ut.I2W[inputs[i]] + " " + ut.I2T[pred] + "\n")
+        pred_file.close()
 
--- utils1.py
+++ utils1.py
@@ -1,14 +1,17 @@
+import random
+
 dir_name = "pos"
 
 TAGS = set()
 WORDS = set()
 
 START = 0  # start of sequence token
-END = -1  # end of sequence token
+END = 1  # end of sequence token
+UNKNOWN_WORD = "*UNKNOWN*"
 
 
 # TRAIN
-def read_data(fname):
+def read_data(fname, unknown_words=False):
     """
     reads the file, and creates lists from each sentence holding tuples (word, tag)
     :param fname: file to read from
@@ -18,7 +21,9 @@ def read_data(fname):
     current = []
     for line in file(fname):
         if line != "\n":
-            word, tag = line[:-1].split(' ')
+            word, tag = line[:-1].split()
+            if unknown_words is True and word not in WORDS:
+                word = UNKNOWN_WORD
             current.append((word, tag))
             WORDS.add(word)
             TAGS.add(tag)
@@ -29,12 +34,12 @@ def read_data(fname):
 
 
 train_raw = read_data(dir_name + "/train")
-dev_raw = read_data(dir_name + "/dev")
+dev_raw = read_data(dir_name + "/dev", unknown_words=True)
 T2I = {tag: i for i, tag in enumerate(list(sorted(TAGS)))}
 I2T = {i: tag for tag, i in T2I.iteritems()}
-W2I = {word: i+1 for i, word in enumerate(list(sorted(WORDS)))}
+W2I = {word: i+2 for i, word in enumerate(list(sorted(WORDS)))}
 W2I[START] = 0
-W2I[END] = -1
+W2I[END] = 1
 I2W = {i: word for word, i in W2I.iteritems()}
 
 
@@ -47,9 +52,10 @@ def make_5_windows(sentences_and_tags):
     """
     windows = []
     for sentence in sentences_and_tags:
-        sentences_and_tags = [START, START] + sentences_and_tags + [END, END]  # adds starts/end tags for start/last win
+        sentence = [(START, START), (START, START)] + sentence + [(END, END), (END, END)]  # adds starts/end tags for start/last win
 
-        for i, (word, tag) in enumerate(sentence[2:-2]):  # from first word, up to n
+        for j, (word, tag) in enumerate(sentence[2:-2]):  # from first word, up to n
+            i = j + 2
             windows.append(([W2I[sentence[i-2][0]], W2I[sentence[i-1][0]], W2I[word],
                                         W2I[sentence[i+1][0]], W2I[sentence[i+2][0]]], T2I[tag]))
     return windows
@@ -58,4 +64,21 @@ def make_5_windows(sentences_and_tags):
 TRAIN = make_5_windows(train_raw)
 DEV = make_5_windows(dev_raw)
 # TEST = make_5_windows(test_raw)
+
+# #########################
+# print DEV[:10]
+# DEV = [([random.randint(0, 10) for _ in range(5)], i % 10) for i in xrange(0, 10000)]
+# print DEV[:10]
+# #########################
+
+if __name__ == "__main__":
+    print "{"
+    for k, v in W2I.iteritems():
+        print " ", k, ":", v
+    print "}"
+    print "{"
+    for k, v in T2I.iteritems():
+        print " ", k, ":", v
+    print "}"
+
 
